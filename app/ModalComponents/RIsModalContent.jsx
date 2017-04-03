d5b84076d4abaa3ae537aba818f2a74580b3e13f
Thomas Welsch <welscht@osti.gov>
2017-04-03 16:35:46 -0400

    RI step now using provided inputs

--- app/ModalComponents/RIsModalContent.jsx
+++ app/ModalComponents/RIsModalContent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import InputHelper from '../components/InputHelper'
-import {observer} from "mobx-react";
+import InputHelper from '../components/PInputHelper'
+import {observer, Provider} from "mobx-react";
 
 
 @observer
@@ -8,12 +8,7 @@ export default class RIsModalContent extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this.onModalChange = this.onModalChange.bind(this);
 	}
-
-    onModalChange(field, value) {
-        this.props.tableStore.setCurrentField(field,value);
-    }
 
 
 	render() {
@@ -33,18 +28,26 @@ export default class RIsModalContent extends React.Component {
 		return(
 
             <div className="container-fluid">
+                <Provider dataStore={ri}>
+                <div>
                 <div className="form-horizontal">
                     {this.props.isEdit && <div className="form-group form-group-sm row">
-                        <InputHelper field="place" label="#" elementType="input" value={ri.place} onChange={this.onModalChange} labelStyle="col-sm-1 control-label" divStyle="col-sm-3"/>
+                        <InputHelper field="place" label="#" elementType="input"/>
                     </div>
                     }
                     <div className="form-group form-group-sm row">
-                        <InputHelper field="identifier_type" label="Identifier Type" elementType="select" options={identifierTypes} value={ri.identifier_type} onChange={this.onModalChange} labelStyle="col-sm-1 control-label" divStyle="col-sm-3"/>
-                        <InputHelper field="relation_type" label="Relation Type" elementType="select" options={relationTypes} value={ri.relation_type} onChange={this.onModalChange} labelStyle="col-sm-1 control-label" divStyle="col-sm-3"/>
-                        <InputHelper field="identifier" label="Identifier" elementType="input" value={ri.identifier} onChange={this.onModalChange} labelStyle="col-sm-1 control-label" divStyle="col-sm-3"/>
+                        <InputHelper field="identifier_type" label="Identifier Type" elementType="select" options={identifierTypes}/>
+                    </div>
+                    <div className="form-group form-group-sm row">
+                        <InputHelper field="relation_type" label="Relation Type" elementType="select" options={relationTypes}  />
+                    </div>
+                    <div className="form-group form-group-sm row">
+                        <InputHelper field="identifier" label="Identifier" elementType="input"/>
                     </div>
 
                 </div>
+                </div>
+                </Provider>
             </div>
 		);
 	}
