28d35ae5057cf168d91f7848411a10471318153e
Thomas Welsch <welscht@osti.gov>
2017-04-03 16:02:54 -0400

    Orgs now using provided inputs, changed style a bit on orgs step to make fields bigger

--- app/ModalComponents/OrgsModalContent.jsx
+++ app/ModalComponents/OrgsModalContent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import InputHelper from '../components/InputHelper'
+import InputHelper from '../components/PInputHelper'
 import Select from 'react-select';
-import {observer} from "mobx-react";
+import {observer,Provider} from "mobx-react";
 
 
 @observer
@@ -9,15 +9,8 @@ export default class OrgsModalContent extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this.onModalChange = this.onModalChange.bind(this);
 	}
-
-    onModalChange(field, value) {
-        this.props.tableStore.setCurrentField(field,value);
-    }
-    
-
-
+	
 	render() {
         const data = this.props.tableStore.currentData();
 		const contributorTypes = [
@@ -34,45 +27,49 @@ export default class OrgsModalContent extends React.Component {
 		return(
 
             <div className="container-fluid">
+            <Provider dataStore={data}>
+            <div>     
                 <div className="form-horizontal">
                     {this.props.isEdit && 
                     
                     <div className="form-group form-group-sm row">
-                        <InputHelper field="place" label="#" elementType="input" value={data.place} onChange={this.onModalChange}/>
+                        <InputHelper divStyle="col-sm-6" field="place" label="#" elementType="input"  />
                     </div>
                     }
                     <div className="form-group form-group-sm row">
-                        <InputHelper field="organization_name" label="Name" elementType="select" value={data.organization_name} allowCreate={true} placeholder="Enter or select from the list your organization" options={orgNames} onChange={this.onModalChange}  />
+                        <InputHelper divStyle="col-sm-6" field="organization_name" label="Name" elementType="select" allowCreate={true} placeholder="Enter or select from the list your organization" options={orgNames}   />
                     </div>
 
                     <div className="form-group form-group-sm row">
-                        <InputHelper field="email" label="Email" elementType="input" value={data.email} onChange={this.onModalChange}/>
+                        <InputHelper divStyle="col-sm-6" field="email" label="Email" elementType="input"  />
                     </div>
                         
                     <div className="form-group form-group-sm row">
-                        <InputHelper field="orcid" label="ORCID" elementType="input" value={data.orcid} onChange={this.onModalChange}/>
+                        <InputHelper divStyle="col-sm-6" field="orcid" label="ORCID" elementType="input"  />
                     </div>
                         
                     {data.primary_award !== undefined &&
                     <div className="form-group form-group-sm row">
-                        <InputHelper field="primary_award" label="Primary Award" elementType="input" value={data.primary_award} onChange={this.onModalChange}/>
+                        <InputHelper divStyle="col-sm-6" field="primary_award" label="Primary Award" elementType="input"  />
                     </div>
                         
                     }
                     
                     {data.primary_award !== undefined &&
                     <div className="form-group form-group-sm row">
-                    <InputHelper field="award_numbers" label="Additional Awards" elementType="select" allowCreate={true} multi={true} placeholder="Enter any additional awards" onChange={this.onModalChange} value={data.award_numbers.slice()} />
+                    <InputHelper divStyle="col-sm-6" field="award_numbers" label="Additional Awards" elementType="select" allowCreate={true} multi={true} placeholder="Enter any additional awards"   />
                     </div>
                     }
 
                     {data.contributor_type !== undefined &&
                     <div className="form-group form-group-sm row">
-                    <InputHelper options={contributorTypes} field="contributor_type" label="Contributor Type" elementType="select" value={data.contributor_type} onChange={this.onModalChange}/>
+                    <InputHelper divStyle="col-sm-6" options={contributorTypes} field="contributor_type" label="Contributor Type" elementType="select"  />
                     </div>
                     }
 
                 </div>
+                </div>
+                </Provider>
             </div>
 		);
 	}
