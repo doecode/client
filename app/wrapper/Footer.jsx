3e3f5997c30235bb42851b99dd9fd3217a9f2f7f
Thomas Welsch <welscht@osti.gov>
2017-07-21 02:10:15 -0400

    Added Footer and Header with Search

--- app/wrapper/Footer.jsx
+++ app/wrapper/Footer.jsx
@@ -2,18 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class Footer extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
+    render() {
 
-render() {
+        const background = {
+            "backgroundColor": "#013588",
+            "marginTop": "10px"
+        };
+        const white = {
+            "color": "white"
+        };
 
+        return (
+            <div style={background}>
+                <div className="container">
 
-  return (
-    <div>
-    </div>
-  );
-}
+                    <div className="row">
+                        <div className="col text-center">
+                            <img src="https://www.osti.gov/scitech/img/ui/DOE_SC_OSTI_70th_white.png" className="mt-3 mb-1" alt="U.S. Department of Energy"/>
+                        </div>
+                    </div>
+                    <div className="row text-center mt-1 mb-1 white">
+                        <a href="#" style={white}>Website Policies / Important Links</a>
+                        &nbsp;•&nbsp;
+                        <a href="#" style={white}>Site Map</a>
+                        &nbsp;•&nbsp;
+                        <a href="#" style={white}>Contact Us</a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
 }
--- app/wrapper/Header.jsx
+++ app/wrapper/Header.jsx
@@ -1,19 +1,87 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchData from '../stores/SearchData';
+
+const searchData = new SearchData();
 export default class Header extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
+        this.state = {all_fields : searchData.getValue("all_fields")};
+        this.onAllFieldsChange = this.onAllFieldsChange.bind(this);
+        this.search = this.search.bind(this);
+    }
+
+    onAllFieldsChange(event) {
+      this.setState({"all_fields" : event.target.value});
+    }
+
+    search() {
+    searchData.clearValues();
+    searchData.setValue("start", 0);
+    searchData.setValue("all_fields", this.state.all_fields);
+    window.sessionStorage.latestSearch = JSON.stringify(searchData.getData());
+    window.location.href = "/results";
+    }
+
+
+
+
+
+    render() {
+
+        const inputStyle = {
+            "paddingLeft": "5px",
+            "fontSize": "20px",
+            "paddingTop": "20px",
+            "paddingBottom": "20px",
+            "marginTop": "5px"
+        };
+
+        const labelStyle = {
+            "fontSize": "19px"
+        };
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+
+                  <div className="mt-1">
+                    <div className="col-xs-offset-1 col-xs-3">
+                        <img className="img-responsive" src="https://www.osti.gov/doecode/images/DOEcode_logo_300px_72ppi.png" alt="DOECode" width="300"/>
+                    </div>
+                    <div className="col-xs-4">
+                        <label for="allSearch" className="sr-only" style={labelStyle}>Search Published Software Entries</label>
+                        <input onChange={this.onAllFieldsChange} type="text" value={this.state.allFields} id="allSearch" className="form-control" style={inputStyle} placeholder="Search DOE Code for Published Software Entries"></input>
+
+                    </div>
+                    <div className="col-xs-4">
+                      <button type="button" className="btn btn-lg btn-success" onClick={this.search}>
+                        <span className="glyphicon glyphicon-search"></span>  Search
+                      </button>
+                    </div>
+                  </div>
+                    <div className="col-xs-offset-1 col-xs-9">
+                        <ul className="nav navbar-nav">
+                            <li>
+                                <a href="#">Software Policy</a>
+                            </li>
+                            <li>
+                                <a href="/wizard">Submit Sofware/Code</a>
+                            </li>
+                            <li>
+                                <a href="#">About</a>
+                            </li>
+                            <li>
+                                <a href="#">Communications/Resources</a>
+                            </li>
+                            <li>
+                                <a href="#">FAQs</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        );
+
+    }
   }
-
-
-render() {
-
-
-  return (
-    <div>
-    </div>
-  );
-}
-
-}
