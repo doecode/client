5d9888a3df7ca03e5c198b62d4476e65e108a7d5
Thomas Welsch <welschtg@lxwelscht2.osti.gov>
2017-08-04 13:40:57 -0400

    Moved file stuff down into supplemental step

--- app/components/EntryStep.jsx
+++ app/components/EntryStep.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import MetadataField from '../field/MetadataField';
-import EntryStepStore from '../stores/EntryStepStore';
 import {observer,Provider} from 'mobx-react';
 import {Button} from 'react-bootstrap';
-import Dropzone from 'react-dropzone';
+import Metadata from '../stores/Metadata';
+import EntryStepStore from '../stores/EntryStepStore';
+
+const metadata = new Metadata();
 
 const entryStore = new EntryStepStore();
 
@@ -13,44 +15,32 @@ export default class EntryStep extends React.Component {
 	constructor(props) {
 		super(props);
 		this.onRadioChange = this.onRadioChange.bind(this);
-		this.onDrop = this.onDrop.bind(this);
-		this.deleteFile = this.deleteFile.bind(this);
+
 	}
-
-
-	onDrop(files) {
-		console.log('Received files: ', files);
-		this.props.metadata.setValue("files", files);
-	}
-
-	deleteFile() {
-		this.props.metadata.setValue("files", []);
-	}
-
 
     onRadioChange(field,value) {
     	let stateObj = this.state;
-    	this.props.metadata.setValue("accessibility",value);
-			const landingPageInfo = this.props.metadata.getFieldInfo("landing_page");
-			const repoLinkInfo = this.props.metadata.getFieldInfo("repository_link");
+    	metadata.setValue("accessibility",value);
+			const landingPageInfo = metadata.getFieldInfo("landing_page");
+			const repoLinkInfo = metadata.getFieldInfo("repository_link");
     	if (value === 'OS') {
-    		this.props.metadata.setValue("open_source", true);
-    		this.props.metadata.setValue("files", []);
-    		this.props.metadata.setValue("landing_page", "");
+    		metadata.setValue("open_source", true);
+    		metadata.setValue("files", []);
+    		metadata.setValue("landing_page", "");
 				repoLinkInfo.required = "pub";
 				landingPageInfo.required = "";
 				repoLinkInfo.Panel = "Repository Information";
 				landingPageInfo.Panel = "";
     	} else if (value === 'ON') {
-    		this.props.metadata.setValue("open_source", true);
-    		this.props.metadata.setValue("repository_link", "");
+    		metadata.setValue("open_source", true);
+    		metadata.setValue("repository_link", "");
 				repoLinkInfo.required = "";
 				landingPageInfo.required = "pub";
 				repoLinkInfo.Panel = "";
 				landingPageInfo.Panel = "Repository Information";
     	} else if (value === 'CS') {
-    		this.props.metadata.setValue("open_source", false);
-    		this.props.metadata.setValue("repository_link", "");
+    		metadata.setValue("open_source", false);
+    		metadata.setValue("repository_link", "");
 				repoLinkInfo.required = "";
 				landingPageInfo.required = "pub";
 				repoLinkInfo.Panel = "";
@@ -65,9 +55,6 @@ export default class EntryStep extends React.Component {
 
 
 	render() {
-		const metadata = this.props.metadata;
-		const repository_link = metadata.getValue("repository_link");
-		const files = metadata.getValue("files");
 		const accessibility = metadata.getValue("accessibility");
 
 		return (
@@ -101,42 +88,6 @@ export default class EntryStep extends React.Component {
 
 		  {(accessibility === 'ON' || accessibility === 'CS')  &&
 	          <MetadataField field="landing_page" label="Landing Page" elementType="input" />
-		  }
-
-
-		  {(accessibility === 'ON' || accessibility === 'CS') &&
-		<div className="form-group form-group-sm row">
-		<div className="col-xs-8">
-		  <label className="form-label">
-		  File Upload
-	      </label>
-		  <div >
-		  		<Dropzone onDrop={this.onDrop}>
-		  		<h2> Drag files here or click to browse. </h2>
-		  		</Dropzone>
-		  </div>
-
-		</div>
-		</div>
-
-
-		  }
-
-		  {files.length > 0 &&
-		 <div className="form-group form-group-sm row">
-		 <label className="col-sm-2">
-		 Uploaded File
-		 </label>
-		 <div className="col-sm-4">
-
-		 {files[0].name}
-		 </div>
-		 <div className="col-sm-4">
-		 <Button bsStyle="danger" active onClick={this.deleteFile}> Delete File </Button>
-		 </div>
-
-		 </div>
-
 		  }
 
 	   </div>
--- app/components/SupplementalInfoStep.jsx
+++ app/components/SupplementalInfoStep.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
+import Dropzone from 'react-dropzone';
+import {Button} from 'react-bootstrap';
 import MetadataField from '../field/MetadataField';
 import {observer,Provider} from "mobx-react";
+import Metadata from '../stores/Metadata';
+
+const metadata = new Metadata();
 
 @observer
 export default class SupplementalInfoStep extends React.Component {
 
 	constructor(props) {
 		super(props);
+		this.onDrop = this.onDrop.bind(this);
+		this.deleteFile = this.deleteFile.bind(this);
 	}
 
+	onDrop(files) {
+		console.log('Received files: ', files);
+		metadata.setValue("files", files);
+	}
+
+	deleteFile() {
+		metadata.setValue("files", []);
+	}
+
+
 	render() {
+
+		const files = metadata.getValue("files");
+		const accessibility = metadata.getValue("accessibility");
 
 		const countries = [
 			{label: 'United States', value: 'United States'},
@@ -29,6 +49,41 @@ export default class SupplementalInfoStep extends React.Component {
 	            	<MetadataField field="keywords" label="Keywords" elementType="input" />
 	            	<MetadataField field="other_special_requirements" label="Other Special Requirements" elementType="input" />
 	              <MetadataField field="site_accession_number" label="Site Accession Number" elementType="input"  />
+
+								{(accessibility === 'ON' || accessibility === 'CS') &&
+							<div className="form-group form-group-sm row">
+							<div className="col-xs-8">
+								<label className="form-label">
+								File Upload
+									</label>
+								<div >
+										<Dropzone onDrop={this.onDrop}>
+										<h2> Drag files here or click to browse. </h2>
+										</Dropzone>
+								</div>
+
+							</div>
+							</div>
+
+
+								}
+
+								{files.length > 0 &&
+							 <div className="form-group form-group-sm row">
+							 <label className="col-sm-2">
+							 Uploaded File
+							 </label>
+							 <div className="col-sm-4">
+
+							 {files[0].name}
+							 </div>
+							 <div className="col-sm-4">
+							 <Button bsStyle="danger" active onClick={this.deleteFile}> Delete File </Button>
+							 </div>
+
+							 </div>
+
+								}
                 </div>
 		);
 	}
