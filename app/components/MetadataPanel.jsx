1d043885304672adb5e37584c2d2bab8491a9c0b
Thomas Welsch <welscht@osti.gov>
2017-08-05 17:39:19 -0400

    Added proprietary URL if proprietary is one of the selected licenses

--- app/components/MetadataPanel.jsx
+++ app/components/MetadataPanel.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import MetadataField from '../field/MetadataField';
 import {observer,Provider} from "mobx-react";
+import Metadata from '../stores/Metadata';
+
+const metadata = new Metadata();
 
 @observer
 export default class MetadataPanel extends React.Component {
 
 	constructor(props) {
 		super(props);
+		this.checkForProp = this.checkForProp.bind(this);
+	}
+
+	checkForProp() {
+
+		const licenses = metadata.getValue("licenses");
+    const hasProp = licenses.indexOf('Proprietary') > -1;
+
+		const propInfo = metadata.getFieldInfo("proprietary_url");
+		if (hasProp) {
+				propInfo.required = "pub";
+				propInfo.Panel = "Product Description";
+		} else {
+			metadata.setValue("proprietary_url", "");
+			propInfo.completed = false;
+			propInfo.required = "";
+			propInfo.Panel = "";
+			propInfo.error = "";
+		}
 	}
 
 	render() {
-		const metadata = this.props.metadata;
 
+		  const propInfo = metadata.getFieldInfo("proprietary_url");
 			const licenseOptions = [
 				{label: 'Proprietary', value: 'Proprietary'},
 				{label: 'Apache License 2.0', value: 'Apache License 2.0'},
@@ -40,8 +62,10 @@ export default class MetadataPanel extends React.Component {
 
 	            	<MetadataField field="software_title" label="Software Title" elementType="input" />
 	            	<MetadataField field="description" label="Description/Abstract" elementType="textarea" />
-					<MetadataField field="licenses" label="Licenses" elementType="select" options={licenseOptions} isArray={true} multi={true} placeholder="Select your license(s)"   />
-
+					<MetadataField field="licenses" label="Licenses" elementType="select" changeCallback={this.checkForProp} options={licenseOptions} isArray={true} multi={true} placeholder="Select your license(s)" />
+					{propInfo.required &&
+						<MetadataField field="proprietary_url" label="Proprietary URL" elementType="input" />
+					}
                 </div>
 		);
 	}
