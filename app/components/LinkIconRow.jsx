aa751f331e23d22ef5591c2863f4c05152c804e6
Thomas Welsch <welschtg@lxwelscht2.osti.gov>
2017-08-10 09:49:10 -0400

    Fixed Splash page Icons bug

--- app/components/LinkIconRow.jsx
+++ app/components/LinkIconRow.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 
-let icon_row = [];
 export default class LinkIconRow extends React.Component{
     constructor(props) {
         super(props);
-        icon_row = this.props.icon_row_data;
     }
-    
+
     render(){
-        const IconList = icon_row.map((row)=>
+        const IconList = this.props.icon_row_data.map((row)=>
            {return row;}
         );
-        
+
         return(
         <div className="row icon-row center-text">
             {IconList}
