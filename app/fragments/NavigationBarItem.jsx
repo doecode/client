de755bfb25fca30c38642c5de4b4aebaa93a9833
smithwa <smitha@osti.gov>
2017-08-11 13:53:00 -0400

    Fixed the issue where the error messages for the password change page weren't showing up Fixed the issue where the icon wasn't showing up on active tabs in the nav

--- app/fragments/NavigationBarItem.jsx
+++ app/fragments/NavigationBarItem.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
 
-let is_active = false;
-let li_class = "";
-let anchor_class = "";
-let fa_icon = "";
 export default class NavgationBarItem extends React.Component{
     constructor(props) {
         super(props);
+        this.is_active = false;
+        this.li_class = "";
+        this.anchor_class = "";
+        this.fa_icon = "";
         
-            is_active = (this.props.destination===this.props.current_page);
-    li_class = (is_active)?" active-menu-item ":"";
+        
+            this.is_active = (this.props.destination===this.props.current_page);
+    this.li_class = (this.is_active)?" active-menu-item ":"";
     
     {/*If it's furthest to the left, we need to add a left side bar*/}
     if(this.props.special !== undefined){
-        li_class+=" nav-menu-item-special ";
+        this.li_class+=" nav-menu-item-special ";
     }
     
-    anchor_class = (is_active)? " nav-menu-item active-menu-item-text " : "nav-menu-item nav-menu-item-text";
-    fa_icon = this.props.fa_icon;
+    this.anchor_class = (this.is_active)? " nav-menu-item active-menu-item-text " : "nav-menu-item nav-menu-item-text";
+    this.fa_icon = this.props.fa_icon;
+    if(this.is_active){
+        this.fa_icon+=" active-menu-item-text ";
+    }
     }
     
     render(){
         return(
-        <li className={li_class}>
-            <a className={anchor_class} href={this.props.destination}><span className={fa_icon}></span> {this.props.display_name}</a>
+        <li className={this.li_class}>
+            <a className={this.anchor_class} href={this.props.destination}><span className={this.fa_icon}></span> {this.props.display_name}</a>
         </li>
         );
     }
