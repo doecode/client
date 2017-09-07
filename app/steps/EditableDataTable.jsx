3d0a889bd3bf2d231423cd600b5aedbbb132891f
smithwa <smithwa@lxsmitha2.osti.gov>
2017-09-07 15:55:44 -0400

    Reverted EditableDataTable

--- app/steps/EditableDataTable.jsx
+++ app/steps/EditableDataTable.jsx
@@ -7,46 +7,50 @@ import Metadata from '../stores/Metadata';
 import {observer} from 'mobx-react';
 import {getChildData} from '../utils/utils'
 
+
 import Griddle from 'griddle-react';
+
 
 const tableStore = new TableStore();
 const metadata = new Metadata();
 
 @observer
 export default class EditableDataTable extends React.Component {
-  constructor(props) {
-    super(props);
-    this.rowClick = this.rowClick.bind(this);
-  }
+    constructor(props) {
+      super(props);
+      this.rowClick = this.rowClick.bind(this);
+    }
 
-  rowClick(gridRow, event) {
-    const parentName = this.props.parentName;
-    const currentArray = metadata.getValue(parentName);
-    const data = getChildData(parentName);
-    const index = currentArray.findIndex(item => item.id === gridRow.props.data.id);
-    data.loadValues(currentArray[index]);
 
-    tableStore.showModal = this.props.parentName;
-    tableStore.isEdit = true;
-  }
+    rowClick(gridRow, event) {
+  	  const parentName = this.props.parentName;
+  	  const currentArray = metadata.getValue(parentName);
+  	  const data = getChildData(parentName);
+  	  const index = currentArray.findIndex(item => item.id === gridRow.props.data.id);
+  	  data.loadValues(currentArray[index]);
 
-  render() {
+  	  tableStore.showModal = this.props.parentName;
+  	  tableStore.isEdit = true;
+    }
 
-    let columns = [];
-    if (this.props.columns !== undefined)
-      columns = this.props.columns;
 
-    const parentName = this.props.parentName;
-    const currentArray = metadata.getValue(parentName);
-    const data = getChildData(parentName);
+	  render() {
 
-    return (
-      <div>
-        <div className="form-group form-group-sm col-sm-12">
-          <Griddle results={currentArray.slice()} columnMetadata={this.props.config} columns={columns} showSettings={true} showFilter={true} onRowClick={this.rowClick}/>
-        </div>
-        <AgentsModal dataType={parentName} tableStore={tableStore} data={data} contentType={this.props.contentType}/>
-      </div>
-    );
-  }
+      let columns = [];
+  	  if (this.props.columns !== undefined)
+  		  columns = this.props.columns;
+
+			const parentName = this.props.parentName;
+			const currentArray = metadata.getValue(parentName);
+			const data = getChildData(parentName);
+
+		    return (
+          <div>
+            <div className="form-group form-group-sm col-sm-12">
+                    <Griddle results = {currentArray.slice()} columnMetadata={this.props.config} columns={columns} showSettings={false} showFilter={true} onRowClick={this.rowClick} />
+            </div>
+           <AgentsModal dataType={parentName} tableStore={tableStore} data={data} contentType={this.props.contentType}  />
+         </div>
+		      );
+		  }
 }
