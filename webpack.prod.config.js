f6935ef00e91a9f0157fdd4c4d2e7fb71c11beab
smithwa <smithwa@lxsmitha2.osti.gov>
2017-10-02 13:18:58 -0400

    Trying a file loader with some new options, as opposed to the url-loader

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -35,7 +35,7 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       }, {
         test: /\.(png|jpg)$/,
-        loader: 'url-loader?limit=8192'
+         loader: 'file-loader?name=/img/[name].[ext]' 
       }
     ]
   },
