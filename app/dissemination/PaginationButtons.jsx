c07fddfb84bcb48bc763c3696eba35f565875bff
sowerst <sowerst@osti.gov>
2017-10-13 10:58:13 -0400

    Make search Pagination dropdown look like OSTI.gov version.

--- app/dissemination/PaginationButtons.jsx
+++ app/dissemination/PaginationButtons.jsx
@@ -19,6 +19,7 @@ export default class PaginationButtons extends React.Component {
   }
 
   toggleDropdown() {
+    console.log("togle");
     this.setState({
       showDropdown: !this.state.showDropdown
     });
@@ -61,25 +62,27 @@ export default class PaginationButtons extends React.Component {
     var isNextDisabled = (this.state.currentPropsVal == this.props.max)
       ? 'disabled'
       : '';
+
+    var isChooseDisabled = (this.state.currentPropsVal == 1 && this.state.currentPropsVal == this.props.max)
+      ? 'disabled'
+      : '';
+
     return (
       <div className='custom-paginate-container'>
-        <button type='button' className='pure-button' title='Previous Page' disabled={isPrevDisabled} onClick={this.goBackAPage}>Prev</button>&nbsp;&nbsp;
-        <button type='button' className='pure-button paginate-slider-arrow' title='Choose a Page' onClick={this.toggleDropdown}>
+        <button type='button' className='pure-button' title='Previous Page' disabled={isPrevDisabled} onClick={this.goBackAPage}><span className="fa fa-angle-left pagination-arrow"></span> Prev</button>&nbsp;&nbsp;
+        <button type='button' className='pure-button paginate-slider-arrow' title='Choose a Page' disabled={isChooseDisabled} onClick={this.toggleDropdown}>
           &hellip;
         </button>&nbsp;&nbsp;
-        <button type='button' className='pure-button' disabled={isNextDisabled} title='Next Page' onClick={this.goForwardAPage}>Next</button>
+        <button type='button' className='pure-button' disabled={isNextDisabled} title='Next Page' onClick={this.goForwardAPage}>Next <span className="fa fa-angle-right pagination-arrow"></span></button>
         {this.state.showDropdown && <div className='pagination-dropdown'>
-          <div className='row'>
-            <div className='col-xs-12 right-text text-muted'>
-              Go to page:&nbsp;{this.state.currentVal}&nbsp;of&nbsp;{this.props.max}
-            </div>
-          </div>
-          <div className="flex-container">
-            <div className='row pagination-row'>
-              <div className='col-xs-9 no-col-padding-right'><input className='pagination-input' onChange={this.showCurrentSliderPage} title={"Page " + this.state.currentVal} type="range" min="1" max={this.props.max} defaultValue={this.state.currentVal} step="1"/></div>
-              <div className='col-xs-3 minimal-col-padding-left left-text'>
-                <button type='button' title={"Go"} className='pure-button pagination-button' onClick={this.goToPage}>»</button>
-              </div>
+          <div id="paging-1" className="pagination-dropdown pagination-dropdown-tip pagination-dropdown-relative pagination-dropdown-style">
+            <div className="pagination-dropdown-panel">
+                <div className="text-muted pagination-go"><label htmlFor="pagination-sel-1">Go to page: <span className="paging-target">{this.state.currentVal}</span> of <span className="paging-max">{this.props.max}</span></label></div>
+                <div><table><tbody><tr><td>
+                    <input data-range="" onChange={this.showCurrentSliderPage} title={"Page " + this.state.currentVal} value={this.state.currentVal} min="1" max={this.props.max} name="pagination-sel" type="range" className="pagination-sel pagination-input" id="pagination-sel-1" step="1" />
+                    </td><td>
+                    <button className="pure-button pagination-go-slider" title={"Go"} type="button" onClick={this.goToPage}>»</button>
+                </td></tr></tbody></table></div>
             </div>
           </div>
         </div>}
