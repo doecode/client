03eb18baa182a45cfddc8661ac6bd58e320a88a6
smithwa <smithwa@lxsmitha2.osti.gov>
2018-01-16 13:12:17 -0500

    Started recaptcha attempt

--- src/main/java/gov/osti/doecode/pagemappings/Other.java
+++ src/main/java/gov/osti/doecode/pagemappings/Other.java
@@ -8,6 +8,7 @@ import gov.osti.doecode.servlet.Init;
 import gov.osti.doecode.utils.JsonObjectUtils;
 import gov.osti.doecode.utils.TemplateUtils;
 import java.io.IOException;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,12 +16,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
 
 public class Other extends HttpServlet {
-
+     
      protected void processRequest(HttpServletRequest request, HttpServletResponse response)
              throws ServletException, IOException {
           String URI = request.getRequestURI();
           String remaining = StringUtils.substringAfterLast(URI, "/" + Init.app_name + "/");
-
+          
           if (StringUtils.equals(request.getContentType(), "application/json")) {
                ObjectNode request_data = JsonObjectUtils.parseObjectNode(request.getReader());
                ObjectNode return_data = new ObjectNode(JsonObjectUtils.FACTORY_INSTANCE);
@@ -29,21 +30,22 @@ public class Other extends HttpServlet {
                          return_data = OtherFunctions.sendGitlabSubmissionEmail(getServletContext(), request_data);
                          break;
                }
-
+               
                JsonObjectUtils.writeTo(return_data, response);
-
+               
           } else {
                String page_title = "";
                String template = "";
                String current_page = "";
                ObjectNode output_data = new ObjectNode(JsonObjectUtils.FACTORY_INSTANCE);
                ArrayNode jsFilesList = new ArrayNode(JsonObjectUtils.FACTORY_INSTANCE);
-
+               ServletContext context = getServletContext();
                switch (remaining) {
                     case "gitlab-signup":
                          page_title = "DOECODE: Gitlab Signup";
                          template = TemplateUtils.TEMPLATE_GITLAB_SIGNUP;
-                         output_data = OtherFunctions.getOtherLists(getServletContext());
+                         output_data = OtherFunctions.getOtherLists(context);
+                         output_data.put("recaptcha_sitekey", context.getInitParameter("recaptcha_sitekey"));
                          break;
                }
 
@@ -65,17 +67,17 @@ public class Other extends HttpServlet {
                TemplateUtils.writeOutTemplateData(page_title, template, response, output_data);
           }
      }
-
+     
      @Override
      protected void doGet(HttpServletRequest request, HttpServletResponse response)
              throws ServletException, IOException {
           processRequest(request, response);
      }
-
+     
      @Override
      protected void doPost(HttpServletRequest request, HttpServletResponse response)
              throws ServletException, IOException {
           processRequest(request, response);
      }
-
+     
 }
