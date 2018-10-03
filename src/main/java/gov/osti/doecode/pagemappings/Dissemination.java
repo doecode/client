654de6ece4e9d83070eebc70613f5219e3ba9ad5
Andrew smith <smithwa@lxsmitha3.osti.gov>
2018-10-03 13:34:14 -0400

    Changed the News/Resources page to be split into two separate pages (One News, One Resources). Added a "News" tab to the navbar.

--- src/main/java/gov/osti/doecode/pagemappings/Dissemination.java
+++ src/main/java/gov/osti/doecode/pagemappings/Dissemination.java
@@ -23,147 +23,158 @@ import org.slf4j.LoggerFactory;
 
 public class Dissemination extends HttpServlet {
 
-     protected Logger log = LoggerFactory.getLogger(Dissemination.class.getName());
+        protected Logger log = LoggerFactory.getLogger(Dissemination.class.getName());
 
-     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-             throws ServletException, IOException {
-          request.setCharacterEncoding("UTF-8");
-          String URI = request.getRequestURI();
-          String remaining = StringUtils.substringAfterLast(URI, "/" + Init.app_name);
+        protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+                        throws ServletException, IOException {
+                request.setCharacterEncoding("UTF-8");
+                String URI = request.getRequestURI();
+                String remaining = StringUtils.substringAfterLast(URI, "/" + Init.app_name);
 
-          String page_title = "";
-          String template = "";
-          String current_page = "";
-          ObjectNode output_data = new ObjectNode(JsonUtils.INSTANCE);
-          ArrayNode jsFilesList = new ArrayNode(JsonUtils.INSTANCE);
+                String page_title = "";
+                String template = "";
+                String current_page = "";
+                ObjectNode output_data = new ObjectNode(JsonUtils.INSTANCE);
+                ArrayNode jsFilesList = new ArrayNode(JsonUtils.INSTANCE);
 
-          /*Determine what page we're on, and load the appropriate title, template, etc*/
-          if (remaining.equals("") || remaining.equals("/")) {//HOMEPAGE
-               page_title = "DOE CODE: Your software services platform and search tool to easily submit, announce, and discover code funded by the U.S. Department of Energy";
-               template = TemplateUtils.TEMPLATE_HOMEPAGE;
+                /*
+                 * Determine what page we're on, and load the appropriate title, template, etc
+                 */
+                if (remaining.equals("") || remaining.equals("/")) {// HOMEPAGE
+                        page_title = "DOE CODE: Your software services platform and search tool to easily submit, announce, and discover code funded by the U.S. Department of Energy";
+                        template = TemplateUtils.TEMPLATE_HOMEPAGE;
 
-          } else if (remaining.equals("/projects")) {
-               page_title = "DOE CODE: Projects";
-               template = TemplateUtils.TEMPLATE_PROJECTS_PAGE;
-               current_page = TemplateUtils.PAGE_PROJECTS;
+                } else if (remaining.equals("/projects")) {
+                        page_title = "DOE CODE: Projects";
+                        template = TemplateUtils.TEMPLATE_PROJECTS_PAGE;
+                        current_page = TemplateUtils.PAGE_PROJECTS;
 
-          } else if (remaining.equals("/repository-services")) {
-               page_title = "DOE CODE: Repository Services";
-               template = TemplateUtils.TEMPLATE_REPOSITORY_SERVICES_PAGE;
-               current_page = TemplateUtils.PAGE_REPOSITORY_SERVICES;
+                } else if (remaining.equals("/repository-services")) {
+                        page_title = "DOE CODE: Repository Services";
+                        template = TemplateUtils.TEMPLATE_REPOSITORY_SERVICES_PAGE;
+                        current_page = TemplateUtils.PAGE_REPOSITORY_SERVICES;
 
-          } else if (remaining.equals("/about")) {
-               page_title = "DOE CODE: About";
-               template = TemplateUtils.TEMPLATE_ABOUT_PAGE;
-               current_page = TemplateUtils.PAGE_ABOUT;
+                } else if (remaining.equals("/about")) {
+                        page_title = "DOE CODE: About";
+                        template = TemplateUtils.TEMPLATE_ABOUT_PAGE;
+                        current_page = TemplateUtils.PAGE_ABOUT;
 
-          } else if (remaining.equals("/policy")) {
-               page_title = "DOE CODE: Policy";
-               template = TemplateUtils.TEMPLATE_POLICY_PAGE;
-               current_page = TemplateUtils.PAGE_POLICY;
+                } else if (remaining.equals("/policy")) {
+                        page_title = "DOE CODE: Policy";
+                        template = TemplateUtils.TEMPLATE_POLICY_PAGE;
+                        current_page = TemplateUtils.PAGE_POLICY;
 
-          } else if (remaining.equals("/news-resources")) {
-               page_title = "DOE CODE: News Resources";
-               template = TemplateUtils.TEMPLATE_NEWS_RESOURCES_PAGE;
-               current_page = TemplateUtils.PAGE_NEWS_RESOURCES;
-               output_data.put("api_url", Init.public_api_url);
+                } else if (remaining.equals("/resources")) {
+                        page_title = "DOE CODE:Resources";
+                        template = TemplateUtils.TEMPLATE_RESOURCES_PAGE;
+                        current_page = TemplateUtils.PAGE_RESOURCES;
+                        output_data.put("api_url", Init.public_api_url);
 
-          } else if (remaining.equals("/faq")) {
-               page_title = "DOE CODE: FAQ's";
-               template = TemplateUtils.TEMPLATE_FAQ_PAGE;
-               current_page = TemplateUtils.PAGE_FAQ;
-               output_data.put("api_url", Init.public_api_url);
+                } else if (remaining.equals("/news")) {
+                        page_title = "DOE CODE: News";
+                        template = TemplateUtils.TEMPLATE_NEWS_PAGE;
+                        current_page = TemplateUtils.PAGE_NEWS;
+                        output_data.put("api_url", Init.public_api_url);
 
-          } else if (remaining.startsWith("/results")) {
-               page_title = "DOE CODE: Search Results";
-               template = TemplateUtils.TEMPLATE_SEARCH_RESULTS_PAGE;
-               String page_num_param = request.getParameter("page");
-               long page_num = StringUtils.isNumeric(page_num_param) ? Long.parseLong(page_num_param) : 1;
-               output_data = SearchFunctions.conductSearch(request, getServletContext(), page_num);
-               jsFilesList.add("libraries/google-chart-loader.min");
+                } else if (remaining.equals("/faq")) {
+                        page_title = "DOE CODE: FAQ's";
+                        template = TemplateUtils.TEMPLATE_FAQ_PAGE;
+                        current_page = TemplateUtils.PAGE_FAQ;
+                        output_data.put("api_url", Init.public_api_url);
 
-          } else if (remaining.equals("/search")) {
-               page_title = "DOE CODE: Advanced Search";
-               template = TemplateUtils.TEMPLATE_ADVANCED_SEARCH;
+                } else if (remaining.startsWith("/results")) {
+                        page_title = "DOE CODE: Search Results";
+                        template = TemplateUtils.TEMPLATE_SEARCH_RESULTS_PAGE;
+                        String page_num_param = request.getParameter("page");
+                        long page_num = StringUtils.isNumeric(page_num_param) ? Long.parseLong(page_num_param) : 1;
+                        output_data = SearchFunctions.conductSearch(request, getServletContext(), page_num);
+                        jsFilesList.add("libraries/google-chart-loader.min");
 
-          } else if (remaining.trim().startsWith("/biblio/")) {
-               template = TemplateUtils.TEMPLATE_BIBLIO_PAGE;
+                } else if (remaining.equals("/search")) {
+                        page_title = "DOE CODE: Advanced Search";
+                        template = TemplateUtils.TEMPLATE_ADVANCED_SEARCH;
 
-               String just_code_id = StringUtils.substringBefore(remaining.replace("/biblio/", ""), "-");
-               if (DOECODEUtils.isValidLong(just_code_id)) {
-                    long code_id = Long.parseLong(just_code_id);
-                    page_title = "DOE CODE: Project Metadata for Code ID " + code_id;
-                    ObjectNode biblio_data = SearchFunctions.getBiblioData(code_id);
+                } else if (remaining.trim().startsWith("/biblio/")) {
+                        template = TemplateUtils.TEMPLATE_BIBLIO_PAGE;
 
-                    //If, and only if, this is a valid code id
-                    if (JsonUtils.getBoolean(biblio_data, "is_valid", false)) {
-                         jsFilesList.add("libraries/clipboard.min");
-                         output_data = biblio_data;
+                        String just_code_id = StringUtils.substringBefore(remaining.replace("/biblio/", ""), "-");
+                        if (DOECODEUtils.isValidLong(just_code_id)) {
+                                long code_id = Long.parseLong(just_code_id);
+                                page_title = "DOE CODE: Project Metadata for Code ID " + code_id;
+                                ObjectNode biblio_data = SearchFunctions.getBiblioData(code_id);
 
-                    } else {
-                         output_data.put("is_invalid_code_id", true);
-                         response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                                // If, and only if, this is a valid code id
+                                if (JsonUtils.getBoolean(biblio_data, "is_valid", false)) {
+                                        jsFilesList.add("libraries/clipboard.min");
+                                        output_data = biblio_data;
 
-                    }
-               } else {
-                    output_data.put("is_invalid_code_id", true);
-                    response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-               }
+                                } else {
+                                        output_data.put("is_invalid_code_id", true);
+                                        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 
-          } else if (remaining.equals("/forbidden")) {
-               page_title = "DOE CODE: Forbidden Access";
-               template = TemplateUtils.TEMPLATE_FORBIDDEN_PAGE;
+                                }
+                        } else {
+                                output_data.put("is_invalid_code_id", true);
+                                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                        }
 
-          } else if (remaining.equals("/security-hosting")) {
-               page_title = "DOE CODE: Security Hosting";
-               template = TemplateUtils.TEMPLATE_SECURITY_HOSTING_PAGE;
+                } else if (remaining.equals("/forbidden")) {
+                        page_title = "DOE CODE: Forbidden Access";
+                        template = TemplateUtils.TEMPLATE_FORBIDDEN_PAGE;
 
-          } else if (remaining.equals("/disclaimer")) {
-               page_title = "DOE CODE: Disclaimer";
-               template = TemplateUtils.TEMPLATE_DISCLAIMER_PAGE;
+                } else if (remaining.equals("/security-hosting")) {
+                        page_title = "DOE CODE: Security Hosting";
+                        template = TemplateUtils.TEMPLATE_SECURITY_HOSTING_PAGE;
 
-          } else if (remaining.equals("/contact")) {
-               page_title = "DOE CODE: Contact Us";
-               template = TemplateUtils.TEMPLATE_CONTACT_PAGE;
+                } else if (remaining.equals("/disclaimer")) {
+                        page_title = "DOE CODE: Disclaimer";
+                        template = TemplateUtils.TEMPLATE_DISCLAIMER_PAGE;
 
-          } else {
-               page_title = "DOE CODE: Page Not Found";
-               template = TemplateUtils.TEMPLATE_404_PAGE;
-          }
+                } else if (remaining.equals("/contact")) {
+                        page_title = "DOE CODE: Contact Us";
+                        template = TemplateUtils.TEMPLATE_CONTACT_PAGE;
 
-          //Add the common dissemination js file
-          jsFilesList.add("dissemination");
+                } else {
+                        page_title = "DOE CODE: Page Not Found";
+                        template = TemplateUtils.TEMPLATE_404_PAGE;
+                }
 
-          //Check if they're logged in, and only do something if they're not logged in
-          if (!UserFunctions.isUserLoggedIn(request)) {
-               output_data.put("user_data", new ObjectNode(JsonUtils.INSTANCE));
-          } else {
-               //Increment time
-               response.addCookie(UserFunctions.updateUserSessionTimeout(request));
-               if (StringUtils.equals(UserFunctions.getOtherUserCookieValue(request, "needs_password_reset"), "true")) {
-                    Cookie needs_reset_cookie = UserFunctions.getOtherUserCookie(request, "needs_password_reset");
-                    needs_reset_cookie.setMaxAge(Init.SESSION_TIMEOUT_MINUTES * 60);
-                    response.addCookie(needs_reset_cookie);
-               }
-          }
+                // Add the common dissemination js file
+                jsFilesList.add("dissemination");
 
-          //Send in this object, and get a hold of the common data, like the classes needed to render the homepage correctly and such
-          output_data = TemplateUtils.GET_COMMON_DATA(output_data, current_page, jsFilesList, null, request);
+                // Check if they're logged in, and only do something if they're not logged in
+                if (!UserFunctions.isUserLoggedIn(request)) {
+                        output_data.put("user_data", new ObjectNode(JsonUtils.INSTANCE));
+                } else {
+                        // Increment time
+                        response.addCookie(UserFunctions.updateUserSessionTimeout(request));
+                        if (StringUtils.equals(UserFunctions.getOtherUserCookieValue(request, "needs_password_reset"),
+                                        "true")) {
+                                Cookie needs_reset_cookie = UserFunctions.getOtherUserCookie(request,
+                                                "needs_password_reset");
+                                needs_reset_cookie.setMaxAge(Init.SESSION_TIMEOUT_MINUTES * 60);
+                                response.addCookie(needs_reset_cookie);
+                        }
+                }
 
-          //Write the template out
-          TemplateUtils.writeOutTemplateData(page_title, template, response, output_data);
-     }
+                // Send in this object, and get a hold of the common data, like the classes
+                // needed to render the homepage correctly and such
+                output_data = TemplateUtils.GET_COMMON_DATA(output_data, current_page, jsFilesList, null, request);
 
-     @Override
-     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-             throws ServletException, IOException {
-          processRequest(request, response);
-     }
+                // Write the template out
+                TemplateUtils.writeOutTemplateData(page_title, template, response, output_data);
+        }
 
-     @Override
-     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-             throws ServletException, IOException {
-          processRequest(request, response);
-     }
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)
+                        throws ServletException, IOException {
+                processRequest(request, response);
+        }
+
+        @Override
+        protected void doPost(HttpServletRequest request, HttpServletResponse response)
+                        throws ServletException, IOException {
+                processRequest(request, response);
+        }
 
 }
