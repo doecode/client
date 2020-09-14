346e609b6bcb1d600e23a78eaf001047301f4c35
Chase Curtis <chasecurtis1991@gmail.com>
2020-09-14 09:02:10 -0400

    Removed "CS,OH" instances, and changed "CS,SH" instances to "Closed Source" (DOECODE-900)

--- src/main/java/gov/osti/doecode/entity/DOECODEJson.java
+++ src/main/java/gov/osti/doecode/entity/DOECODEJson.java
@@ -30,7 +30,7 @@ public class DOECODEJson {
     public static final String PROGRAMMING_LANGUAGES_KEY = "programming_languages";
     public static final String PROJECT_KEYWORDS_KEY = "project_keywords";
 
-    //Json Array lists
+    // Json Array lists
     private ArrayNode affiliations_list;
     private ArrayNode availability_list;
     private ArrayNode contributor_types;
@@ -63,17 +63,21 @@ public class DOECODEJson {
         this.relation_types_list = JsonUtils.MAPPER.createArrayNode();
         this.project_keywords_list = JsonUtils.MAPPER.createArrayNode();
 
-        //Since some of the lists have content that isn't pulled from OSTI's Elink Authority API, we'll go ahead and fill those lists up
-        //Search Sort Options
+        // Since some of the lists have content that isn't pulled from OSTI's Elink
+        // Authority API, we'll go ahead and fill those lists up
+        // Search Sort Options
         this.search_sort_options_list.add(makeListObj("Relevance", "score desc", "Relevance"));
-        this.search_sort_options_list.add(makeListObj("Release Date (newest to oldest)", "releaseDate desc", "Release Date (newest to oldest)"));
-        this.search_sort_options_list.add(makeListObj("Release Date (oldest to newest)", "releaseDate asc", "Release Date (oldest to newest)"));
-        //Availability
-        this.availability_list.add(makeListObj("Open Source, Publicly Available Repository", "OS", "Open Source, Publicly Available Repository"));
-        this.availability_list.add(makeListObj("Open Source, No Publicly Available Repository", "ON", "Open Source, No Publicly Available Repository"));
-        this.availability_list.add(makeListObj("Closed Source, Site Hosted", "CS", "Closed Source, Site Hosted"));
-        this.availability_list.add(makeListObj("Closed Source, OSTI Hosted", "CO", "Closed Source, OSTI Hosted"));
-        //Contributor Types
+        this.search_sort_options_list.add(
+                makeListObj("Release Date (newest to oldest)", "releaseDate desc", "Release Date (newest to oldest)"));
+        this.search_sort_options_list.add(
+                makeListObj("Release Date (oldest to newest)", "releaseDate asc", "Release Date (oldest to newest)"));
+        // Availability
+        this.availability_list.add(makeListObj("Open Source, Publicly Available Repository", "OS",
+                "Open Source, Publicly Available Repository"));
+        this.availability_list.add(makeListObj("Open Source, No Publicly Available Repository", "ON",
+                "Open Source, No Publicly Available Repository"));
+        this.availability_list.add(makeListObj("Closed Source", "CS", "Closed Source"));
+        // Contributor Types
         this.contributor_types.add(makeListObj("Contact Person", "ContactPerson", "Contact Person"));
         this.contributor_types.add(makeListObj("Data Collector", "DataCollector", "Data Collector"));
         this.contributor_types.add(makeListObj("Data Curator", "DataCurator", "Data Curator"));
@@ -87,7 +91,8 @@ public class DOECODEJson {
         this.contributor_types.add(makeListObj("Project Manager", "ProjectManager", "Project Manager"));
         this.contributor_types.add(makeListObj("Project Member", "ProjectMember", "Project Member"));
         this.contributor_types.add(makeListObj("Registration Agency", "RegistrationAgency", "Registration Agency"));
-        this.contributor_types.add(makeListObj("Registration Authority", "RegistrationAuthority", "Registration Authority"));
+        this.contributor_types
+                .add(makeListObj("Registration Authority", "RegistrationAuthority", "Registration Authority"));
         this.contributor_types.add(makeListObj("Related Person", "RelatedPerson", "Related Person"));
         this.contributor_types.add(makeListObj("Research Group", "ResearchGroup", "Research Group"));
         this.contributor_types.add(makeListObj("Researcher", "Researcher", "Researcher"));
@@ -95,25 +100,35 @@ public class DOECODEJson {
         this.contributor_types.add(makeListObj("Sponsor", "Sponsor", "Sponsor"));
         this.contributor_types.add(makeListObj("Supervisor", "Supervisor", "Supervisor"));
         this.contributor_types.add(makeListObj("Work Package Leader", "WorkPackageLeader", "Work Package Leader"));
-        //License Options
+        // License Options
         this.license_options_list.add(makeListObj("Other (Commercial or Open-Source)", "Other", "Other"));
         this.license_options_list.add(makeListObj("Apache License 2.0", "Apache License 2.0", "Apache License 2.0"));
-        this.license_options_list.add(makeListObj("GNU General Public License v3.0", "GNU General Public License v3.0", "GNU General Public License v3.0"));
+        this.license_options_list.add(makeListObj("GNU General Public License v3.0", "GNU General Public License v3.0",
+                "GNU General Public License v3.0"));
         this.license_options_list.add(makeListObj("MIT License", "MIT License", "MIT License"));
-        this.license_options_list.add(makeListObj("BSD 2-clause \"Simplified\" License", "BSD 2-clause \"Simplified\" License", "BSD 2-clause \"Simplified\" License"));
-        this.license_options_list.add(makeListObj("BSD 3-clause \"New\" or \"Revised\" License", "BSD 3-clause \"New\" or \"Revised\" License", "BSD 3-clause \"New\" or \"Revised\" License"));
-        this.license_options_list.add(makeListObj("Eclipse Public License 1.0", "Eclipse Public License 1.0", "Eclipse Public License 1.0"));
-        this.license_options_list.add(makeListObj("GNU Affero General Public License v3.0", "GNU Affero General Public License v3.0", "GNU Affero General Public License v3.0"));
-        this.license_options_list.add(makeListObj("GNU General Public License v2.0", "GNU General Public License v2.0", "GNU General Public License v2.0"));
-        this.license_options_list.add(makeListObj("GNU General Public License v2.1", "GNU General Public License v2.1", "GNU General Public License v2.1"));
-        this.license_options_list.add(makeListObj("GNU Lesser General Public License v2.1", "GNU Lesser General Public License v2.1", "GNU Lesser General Public License v2.1"));
-        this.license_options_list.add(makeListObj("GNU Lesser General Public License v3.0", "GNU Lesser General Public License v3.0", "GNU Lesser General Public License v3.0"));
-        this.license_options_list.add(makeListObj("Mozilla Public License 2.0", "Mozilla Public License 2.0", "Mozilla Public License 2.0"));
+        this.license_options_list.add(makeListObj("BSD 2-clause \"Simplified\" License",
+                "BSD 2-clause \"Simplified\" License", "BSD 2-clause \"Simplified\" License"));
+        this.license_options_list.add(makeListObj("BSD 3-clause \"New\" or \"Revised\" License",
+                "BSD 3-clause \"New\" or \"Revised\" License", "BSD 3-clause \"New\" or \"Revised\" License"));
+        this.license_options_list.add(
+                makeListObj("Eclipse Public License 1.0", "Eclipse Public License 1.0", "Eclipse Public License 1.0"));
+        this.license_options_list.add(makeListObj("GNU Affero General Public License v3.0",
+                "GNU Affero General Public License v3.0", "GNU Affero General Public License v3.0"));
+        this.license_options_list.add(makeListObj("GNU General Public License v2.0", "GNU General Public License v2.0",
+                "GNU General Public License v2.0"));
+        this.license_options_list.add(makeListObj("GNU General Public License v2.1", "GNU General Public License v2.1",
+                "GNU General Public License v2.1"));
+        this.license_options_list.add(makeListObj("GNU Lesser General Public License v2.1",
+                "GNU Lesser General Public License v2.1", "GNU Lesser General Public License v2.1"));
+        this.license_options_list.add(makeListObj("GNU Lesser General Public License v3.0",
+                "GNU Lesser General Public License v3.0", "GNU Lesser General Public License v3.0"));
+        this.license_options_list.add(
+                makeListObj("Mozilla Public License 2.0", "Mozilla Public License 2.0", "Mozilla Public License 2.0"));
         this.license_options_list.add(makeListObj("The Unlicense", "The Unlicense", "The Unlicense"));
-        //Software Type
+        // Software Type
         this.software_type_list.add(makeListObj("Business", "B", "Business"));
         this.software_type_list.add(makeListObj("Scientific", "S", "Scientific"));
-        //States List
+        // States List
         this.states_list.add(makeListObj("", "", ""));
         this.states_list.add(makeListObj("Alabama", "AL", "Alabama"));
         this.states_list.add(makeListObj("Alaska", "AK", "Alaska"));
@@ -180,7 +195,7 @@ public class DOECODEJson {
     public final void UPDATE_REMOTE_LISTS(ServletContext context) throws Exception {
         String authorityapi_base_url = Init.authority_api_base;
 
-        //Countries
+        // Countries
         ArrayNode country = getItemFromElinkAuthority(authorityapi_base_url + "simple/countries-list?sort=description");
         if (country.size() > 0) {
             this.countries_list = translateElinkAuthorityList(country);
@@ -188,9 +203,9 @@ public class DOECODEJson {
             log.error("Country Json Array returned empty");
         }
 
-        //Sponsoring Orgs
+        // Sponsoring Orgs
         ArrayNode sponsoring_orgs = JsonUtils.MAPPER.createArrayNode();
-        //These two items have to be added to the top of the sponsoring org list
+        // These two items have to be added to the top of the sponsoring org list
         ObjectNode blank = JsonUtils.MAPPER.createObjectNode();
         blank.put("name", "");
         blank.put("code", "");
@@ -201,7 +216,7 @@ public class DOECODEJson {
         blank.put("status", "C");
         sponsoring_orgs.add(blank);
         sponsoring_orgs.add(usdoe);
-        //Get the rest of the sponsoring org items
+        // Get the rest of the sponsoring org items
         ArrayNode sponsor_orgs_api = getItemFromElinkAuthority(authorityapi_base_url + "sponsor/sponsor-org-list");
         if (sponsor_orgs_api.size() > 0) {
             sponsoring_orgs = translateElinkAuthorityList(sponsoring_orgs);
@@ -217,7 +232,7 @@ public class DOECODEJson {
             log.error("Sponsoring Orgs Json Array returned empty");
         }
 
-        //Research Org
+        // Research Org
         ArrayNode research_orgs = getItemFromElinkAuthority(authorityapi_base_url + "research/orig-research-org-list");
         if (research_orgs.size() > 0) {
             this.research_org_list = translateElinkAuthorityList(research_orgs);
@@ -225,7 +240,7 @@ public class DOECODEJson {
             log.error("Research Orgs Json Array returned empty");
         }
 
-        //Affiliations
+        // Affiliations
         ArrayNode affiliations = getItemFromElinkAuthority(authorityapi_base_url + "affiliations/affiliations-list");
         if (affiliations.size() > 0) {
             this.affiliations_list = translateElinkAuthorityList(affiliations);
@@ -233,24 +248,27 @@ public class DOECODEJson {
             log.error("Affiliations Json Array returned empty");
         }
 
-        //Relation Types
-        ArrayNode relation_types = getItemFromElinkAuthority(authorityapi_base_url + "simple/relation-types-list?status=C");
+        // Relation Types
+        ArrayNode relation_types = getItemFromElinkAuthority(
+                authorityapi_base_url + "simple/relation-types-list?status=C");
         if (relation_types.size() > 0) {
             this.relation_types_list = translateElinkAuthorityList(relation_types);
         } else {
             log.error("Relation Types Json Array returned empty");
         }
 
-        //Programming Languages
-        ArrayNode programming_languages = getItemFromElinkAuthority(authorityapi_base_url + "simple/programming-languages-list");
+        // Programming Languages
+        ArrayNode programming_languages = getItemFromElinkAuthority(
+                authorityapi_base_url + "simple/programming-languages-list");
         if (programming_languages.size() > 0) {
             this.programming_languages_list = translateElinkAuthorityList(programming_languages);
         } else {
             log.error("Programming Languages Json Array returned empty");
         }
 
-        //Project Keywords
-        ArrayNode project_keywords = getItemFromElinkAuthority(authorityapi_base_url + "simple/doecode-project-keywords-list");
+        // Project Keywords
+        ArrayNode project_keywords = getItemFromElinkAuthority(
+                authorityapi_base_url + "simple/doecode-project-keywords-list");
         if (project_keywords.size() > 0) {
             this.project_keywords_list = translateElinkAuthorityList(project_keywords);
         } else {
